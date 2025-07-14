da19585bc2ffd38562ecfa1fd166ecd3faea7498
Sollos <Chaun.likegames@gmail.com>
2025-07-14 06:32:10 +0400

    Чтобы легче добавлять было в БД

--- src/gameCreater.php
+++ src/gameCreater.php
@@ -1,6 +1,6 @@
 <?php
-
-require_once __DIR__ . '\helpr.php' ;
+//НЕ ЗАПУСКАТЬ!!!
+//Используется для заполнения БД ВРУЧНУЮ!!!
 
 $name = "Atomic Heart";
 $grade = "10";
@@ -9,16 +9,5 @@ $creationDate = date("d-m-Y", strtotime("2023-02-21"));
 $description = "Масштабный сбой системы на Советском Предприятии №3826 привел к восстанию машин против человечества. Вы — Майор P-3, ваша задача — ликвидировать последствия глобальной катастрофы и сдержать утечку засекреченной информации, угрожающей уничтожением всему миру.";
 $imageUrl = "/Images/Atomic_Heart.webp";
 
-$connect = getDb();
-
-$sql = "INSERT INTO `users` (login , password, email) VALUES ('$login', '$password', '$email')";
-
-$result = $connect->query($sql);
-
-If ($connect -> query($sql) === TRUE) {
- echo 'игра добавленна';
-} else {
- echo 'не работает';
-}
 
 ?>
